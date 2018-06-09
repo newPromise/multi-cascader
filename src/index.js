7fed1b3a85d9d06318506a686cc667ac60cffd4d
znn <250621458@qq.com>
2018-06-09 20:09:00 +0800

    修复不能正常安装问题

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import Vue from "vue";
 const install = (Vue) => {
     Vue.component(multiCascader.name, multiCascader)
 };
-
+if (typeof window !== 'undefined' && window.Vue) {
+    install(window.Vue);
+}
 export { multiCascader, install };
