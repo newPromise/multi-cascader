c6b27ee6c55341d4753d773a8d5dc0361a94d7a9
znn <250621458@qq.com>
2018-06-09 19:36:26 +0800

    重新编译

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import multiCascader from "./components";
+import Vue from "vue";
 
 const install = (Vue) => {
     Vue.component(multiCascader.name, multiCascader)
 };
+
+install(Vue);
 
 export default { multiCascader, install };
