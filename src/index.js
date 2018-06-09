2c4535933eeb59587e586c4b7e85aa2974e5c7ae
znn <250621458@qq.com>
2018-06-09 19:28:51 +0800

    修复不能正常安装问题

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import multiCascader from "./components";
-import Vue from "vue";
 
 const install = (Vue) => {
     Vue.component(multiCascader.name, multiCascader)
 };
-
-install(Vue);
 
 export default { multiCascader, install };
