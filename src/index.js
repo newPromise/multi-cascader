f1f020f656b4383c2efe24625c8321ab95da9d13
znn <250621458@qq.com>
2018-06-09 18:06:29 +0800

    添加安装

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import multiCascader from "./components";
 import Vue from "vue";
 
-const install = () => {
+const install = (Vue) => {
     Vue.component(multiCascader.name, multiCascader)
 };
+
+install(Vue);
 
 export default multiCascader;
