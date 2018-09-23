5140c688a99a460bffe6a8012d32613a2d2c2dd8
newPromise <250621458@qq.com>
2018-09-23 18:15:03 +0800

    解决初始化选中选项错误问题

--- src/components/MulCheckCascader.vue
+++ src/components/MulCheckCascader.vue
@@ -1,14 +1,24 @@
 <template lang='html'>
-    <div class='vk-multil-cascader'>
-        <el-popover placement="top-start" popper-class="vk-multi-cascader-popover" :visible-arrow="showArrow" trigger="click">
-            <muContent :height="height" :width="width" :option="options" @handleOutPut="whenOutPut"></muContent>
-            <el-input popper-class="slect-panel" v-if="activeItem[0] && activeItem[0].level === 0"  v-model="inputValue" readonly slot="reference"></el-input>
+    <div class='multil-cascader'>
+        <!-- <el-input/>/ -->
+        <el-popover placement="top-start" popper-class="multi-cascader-popover" :visible-arrow="showArrow" trigger="click" @hide="whenPopoverHide" @show="whenPopoverShow">
+            <muContent
+                :height="height"
+                :width="width"
+                :option="options"
+                @handleOutPut="whenOutPut"
+                :selectedValues="selectedValues"
+                :outputType="outputType"
+                :disabledPair="disabledPair">
+            </muContent>
+            <el-input popper-class="slect-panel" v-if="activeItem[0] && activeItem[0].level === 0"  v-model="inputValue" readonly slot="reference" :suffix-icon="inputArrow"/>
         </el-popover>
     </div>
 </template>
 
 <script>
 import muContent from "./multiContent";
+// 基于 vue.js && element-ui 的多选级联选择器
 export default {
     name: "multiCascader",
     props: {
@@ -31,32 +41,112 @@ export default {
             default() {
                 return "";
             }
+        },
+        // 输出值的类型
+        outputType: {
+            type: String,
+            default() {
+                return "value";
+            }
+        },
+        // 互斥对儿
+        disabledPair: {
+            type: Object,
+            default() {
+                return {};
+            }
         }
     },
     data() {
         return {
+            // 被选中的值
+            selectedValues: [],
             showArrow: true,
             activeItem: [],
             outputValue: [],
             optionDicts: [],
-            popoverStyle: {
-            }
+            inputArrow: "el-icon-arrow-down",
+            popoverWidth: "",
+            // 展开之后的数组， 将每一个children 展开
+            flatOptions: []
         };
+    },
+    watch: {
+        "options": function () {
+            this.initData();
+        }
     },
     components: {
         muContent
     },
-    computed: {},
     created() {
         this.initData();
-        this.setLevel();
         this.setOptionDicts(this.options);
+        this.toFlatOption(this.options);
     },
     methods: {
+        whenPopoverHide() {
+            this.inputArrow = "el-icon-arrow-down";
+        },
+        whenPopoverShow() {
+            this.inputArrow = "el-icon-arrow-up";
+        },
+        // 初始化数据 对于每一项 options 添加相关字段并且获取到当前被点击到的元素
         initData() {
-            this.activeItem = this.options;
+            this.setLevel();
             const { width, height } = this;
-            this.popoverStyle = Object.assign({}, { width, height });
+            const checkedValues = [];
+            let childrenValues = [];
+            const getChecked = (item) => {
+                const { checked, value, children, level, siblingValues } = item;
+                if (siblingValues) {
+                    const tempValues = [...siblingValues];
+                    item.siblingValues = tempValues;
+                }
+                childrenValues.push(value);
+                if (children && children.length > 0) {
+                    children.forEach(child => {
+                        getChecked(child);
+                    });
+                } else {
+                    if (checked && item[this.outputType]) checkedValues.push(item[this.outputType]);
+                }
+            };
+            this.activeItem = this.options;            
+            this.options.forEach(child => {
+                getChecked(child);
+                // 设置当前item 的 childrenValues, 包含当前item 下的所有值的 value
+                child.childrenValues = [...childrenValues];
+                childrenValues = [];
+            });
+            this.selectedValues = checkedValues;
+            this.whenOutPut(this.selectedValues);
+        },
+        getTypeOptions(values, outputType) {
+            const outputValues = [...values];
+            const finalOutputArr = [];
+            return this.flatOptions.reduce((pev, cur) => {
+                const { value: curVal } = cur;
+                if (outputType === "item") {
+                    if (outputValues.includes(curVal)) pev.push(cur);
+                } else {
+                    if (outputValues.includes(curVal) && cur[outputType]) pev.push(cur[outputType]);
+                }
+                return pev;
+            }, []);
+        },
+        // 展开配置中的各项， [{}], 排除 children 属性
+        toFlatOption(option) {
+            const getItems = (arr, cur) => {
+                const keys = Object.keys(cur);
+                const newObj = {};
+                const curChild = cur.children;
+                const hasChild = curChild && curChild.length > 0;          
+                keys.forEach(key => key !== "children" && (newObj[key] = cur[key]));
+                arr.push(newObj);
+                return (hasChild ? curChild.reduce(getItems, arr) : arr);
+            };
+            this.flatOptions = option.reduce(getItems, []);
         },
         // 设置配置的字典
         setOptionDicts(options) {
@@ -73,15 +163,26 @@ export default {
                 });
             }
         },
+        // 触发 on-selected 事件
         whenOutPut(value) {
-            this.outputValue = value;
-            this.$emit("on-selected", value);
+            // 根据选中的值数组 value 输出特定 outputType 类型
+            if (this.outputType !== "value") {
+                this.outputValue = this.getTypeOptions(value, this.outputType);
+            } else {
+                this.outputValue = value;
+            }
+            this.$emit("on-selected", this.outputValue);
         },
         // 设定层级
         setLevel() {
+            const siblingValues = [];
+            let tempLevel = 0;
             if (this.options.length) {
                 const addLevel = option => {
                     const optChild = option.children;
+                    if (option.level === tempLevel) {
+                        siblingValues.push(option.value);
+                    }
                     if (optChild) {
                         optChild.forEach(opt => {
                             opt.level = option.level + 1;
@@ -92,8 +193,10 @@ export default {
                 this.options.forEach(option => {
                     if (!option.level) {
                         option.level = 0;
+                        tempLevel = option.level;
                     }
                     addLevel(option);
+                    option.siblingValues = siblingValues;
                 });
             }
         },
--- src/components/multiContent.vue
+++ src/components/multiContent.vue
@@ -1,11 +1,12 @@
 <template lang="html">
     <div class="popver-content">
-        <div class="vk-multil-content" :style="contentStyle">
-            <ul class="vk-multi-menu">
-                <li class="vk-menu-item" v-for="(item, index) of option"
+        <div class="multiCascader-multil-content" :style="contentStyle">
+            <ul class="multiCascader-multi-menu">
+                <li v-for="(item, index) of option"
                     :key="index"
+                    :class="[ 'multiCascader-menu-item', { 'item-disabled': item.disabled }]"
                     @click="showNextLevel(item)">
-                    <el-checkbox v-model="item.checked" @change="checkChange(item)">{{ item.label }}</el-checkbox>
+                    <el-checkbox :disabled="item.disabled" v-model="item.checked" @change="checkChange(item)">{{ item.label }}</el-checkbox>
                     <i class="el-icon-arrow-right" v-show="item.children && item.children.length > 0"></i>
                 </li>
             </ul>
@@ -18,12 +19,15 @@
             v-if="(activeItem && activeItem.children) && (activeItem.children.length > 0)"
             :selectedValues="selectedValues"
             @handleOutPut="whenOutPut"
+            :disabledPair="disabledPair"
             :option="activeItem.children" >
         </muContent>
     </div>
 </template>
 
 <script>
+const vm = this;
+import Vue from "vue";
 export default {
     name: "muContent",
     props: {
@@ -33,12 +37,14 @@ export default {
                 return [];
             }
         },
+        // 被选中的值
         selectedValues: {
             type: Array,
             default() {
                 return [];
             }
         },
+        // 设置的宽度
         width: {
             type: String,
             default: ""
@@ -46,6 +52,13 @@ export default {
         height: {
             type: String,
             default: ""
+        },
+        // 禁用字段
+        disabledPair: {
+            type: Object,
+            default() {
+                return {};
+            }
         }
     },
     data() {
@@ -55,11 +68,14 @@ export default {
             contentStyle: {
                 width: "",
                 height: ""
-            }
+            },
+            checkArr: [],
+            checkDisabled: false
         };
     },
     created() {
         this.initData();
+        this.whenOutPut(this.selectedValues);
     },
     methods: {
         // 逐级上传
@@ -68,55 +84,103 @@ export default {
         },
         initData() {
             const { width, height } = this;
-            this.contentStyle = Object.assign(this.contentStyle, { width, height });
+            this.contentStyle = Object.assign({}, this.contentStyle, { width, height });
         },
         // 获取到选中的值
         checkChange(item) {
-            const that = this;
-            if (!item.children || item.children.length === 0) {
-                if (this.selectedValues.includes(item.value)) {
-                    const index = this.selectedValues.indexOf(item.value);
-                    this.selectedValues.splice(index, 1);
-                } else {
-                    this.selectedValues.push(item.value);
-                }
-            }
-            const setChecked = toCheckItem => {
-                toCheckItem.checked = item.checked;
-                const { value, checked } = toCheckItem;
-                const getValIndex = this.selectedValues.findIndex(val => val === value);
-                if (checked) {
+            const getCheckedItems = item => {
+                const { value, checked, level } = item;
+                if (checked && level) {
                     this.selectedValues.push(value);
-                } else if (getValIndex >= 0) {
-                    this.selectedValues.splice(getValIndex, 1);
+                } else if (!checked) {
+                    item.disabled = false;
+                    if (this.selectedValues.includes(value)) {
+                        this.selectedValues.splice(this.selectedValues.findIndex(slectVal => slectVal === value), 1);
+                    }
                 }
-                const itemChild = toCheckItem.children;
-                if (itemChild && itemChild.length > 0) {
-                    itemChild.forEach(child => {
-                        setChecked(child);
-                    });
+                const itemChild = item.children;
+                if (itemChild) {
+                    itemChild.forEach(child => child.checked = checked);
                 }
             };
-            // 当一级菜单改变的时候
-            if (item.children) {
-                item.children.forEach(child => setChecked(child));
-            }
+            
+            this.recursiveFn(item, getCheckedItems);
+            this.disabeldAction(item);
             this.activeItem = item;
             this.$emit("handleSelect", this.option);
             this.$emit("handleOutPut", this.selectedValues);
         },
+        // 当二级菜单改变的时候
         whenSelected(val) {
-            let hasCheckTrue = true;
+            let allChildCancelChecked = true;
             if (Array.isArray(val) && val.length > 0) {
-                hasCheckTrue = val.every(child => child.checked === false);
+                allChildCancelChecked = val.every(child => child.checked === false);
             }
-            if (hasCheckTrue) {
-                this.activeItem.checked = false;
+            this.activeItem.checked = !allChildCancelChecked;
+            this.disabeldAction(this.activeItem);
+            this.$emit("handleSelect", this.option);
+        },
+        // 递归函数
+        recursiveFn(curItem, cb) {
+            cb(curItem);
+            const children = curItem.children;
+            if (children && children.length > 0) {
+                children.forEach(item => {
+                    this.recursiveFn(item, cb);
+                });
+            }
+        },
+        // 设置 disabled 值 values: 互斥的另一方数组， curItem 当前选中的值
+        setDisabled(exceptValues, curItem, values) {
+            const { checked: curChecked, childrenValues, value: curValue, siblingValues } = curItem;
+            this.checkArr = [];
+            if (values.includes("all")) {
+                if (siblingValues) {
+                    this.checkArr = new Array(siblingValues.length - exceptValues.length).fill(true);
+                }
             } else {
-                this.activeItem.checked = true;
+                this.checkArr = new Array(values.length).fill(true);
+            }
+            const getCheckArr = item => {
+                const { value, checked } = item;
+                if (!exceptValues.includes(value)) return;
+                this.checkArr.push(checked);
+                this.checkArr.shift();          
+            };
+            const resetDistable = (child) => {
+                if (!values.includes(child.value)) return;
+                child.disabled = this.checkArr.some(val => val === true);
+            }
+            this.option.forEach(opt => {
+                this.recursiveFn(opt, getCheckArr);
+            });
+            this.option.forEach(opt => {
+                this.recursiveFn(opt, resetDistable);
+            });
+        },
+        // disabled action
+        // 根据选中的值进行设置是否可选
+        disabeldAction(item) {
+            const { thatPair, thisPair } = this.disabledPair;
+            if (!thatPair || !thisPair) {
+                return;
+            }
+            const pairs = [...thatPair, ...thisPair];
+            const { value: itemVal } = item;
+            const belongPair = pairs.includes(itemVal) || pairs.includes("all");
+            let distableValues = [];
+            let ableValues = [];
+            if (!belongPair) return;
+            if (thisPair.includes(item.value) || (thisPair.includes("all") && !thatPair.includes(item.value))) {
+                this.setDisabled(thisPair, item, thatPair);
+                return;
+            }
+            if (thatPair.includes(item.value) || (thatPair.includes("all") && !thisPair.includes(item.value))) {
+                this.setDisabled(thatPair, item, thisPair);
             }
         },
         showNextLevel(item) {
+            if (item.disabled) return;
             this.activeItem = item;
         }
     }
@@ -127,13 +191,10 @@ export default {
     display: flex;
     justify-content: space-between;
 }
-.vk-multil-content {
-    width: 220px;
-    height: 240px;
+.multiCascader-multil-content {
     display: inline-block;
-    overflow: auto;
 }
-.vk-menu-item {
+.multiCascader-menu-item {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -145,9 +206,12 @@ export default {
     outline: none;
     padding: 8px 20px;
     font-size: 14px;
-    width: 100%;
     &:hover {
         background-color: rgba(125,139,169,.1);
     }
+}
+.item-disabled {
+    color: #c0c4cc;
+    cursor: not-allowed;
 }
 </style>
